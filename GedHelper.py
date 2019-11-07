4d75749d9f198f8e8a813f477b3c8d65d0b9808e
Samarpan Biswas <sb6165@nyu.edu>
2019-11-06 20:23:20 -0500

    Added methods for US05 and US06
    Added methods in GedHelper.py and checked the condition in GedRead.py

---
+++ GedHelper.py
@@ -0,0 +1,215 @@
+__author__= 'Group5'
+
+import datetime
+from GedMembers import Valid
+from GedMembers import individual
+from GedMembers import family
+from GedUtil import gedUtil
+
+class gedHelper(object):
+
+
+    def __init__(self):
+        pass
+    #US01 Date before CurrentDate
+    def datebeforeCurrentdate(self, person):
+        util = gedUtil()
+        current = datetime.datetime.today()
+        if (person.birth !="not mentioned"):
+            return gedUtil().dateCompare(util.getDate(current),person.birth)
+        elif (person.marDate !="not mentioned"):
+            return gedUtil().dateCompare(person.marDate,util.getDate(current))
+        elif (person.death !="not mentioned"):
+            return gedUtil().dateCompare(person.death,util.getDate(current))
+        elif (person.divDate !="not mentioned"):
+            return gedUtil().dateCompare(person.divDate,util.getDate(current))
+        else:
+            return True
+
+    #US02 Birth before Marriage
+    def birthBeforeMarriage(self, person):
+        if (person.birth =="not mentioned")or(person.marDate =="not mentioned"):
+            return True
+        else:
+            return gedUtil().dateCompare(person.marDate,person.birth)
+
+    #US03 Birth before death
+    def birthBeforeDeath(self, person):
+        if (person.birth =="not mentioned")or(person.death =="not mentioned"):
+            return True
+        else:
+            return gedUtil().dateCompare(person.death,person.birth)
+
+    #US04 Marriage before divorce
+    def marriageBeforeDivorce(self, person):
+        if (person.marDate =="not mentioned")or(person.divDate =="not mentioned"):
+            return True
+        else:
+            return gedUtil().dateCompare(person.divDate,person.marDate)
+	
+	
+	
+	#US05 Marriage before death
+    def marriageBeforeDeath(self, person):
+        if (person.marDate =="not mentioned")or(person.death =="not mentioned"):
+            return True
+        else:
+            return gedUtil().dateCompare(person.death,person.marDate)
+			
+			
+	#US06 Divorce before death
+    def marriageBeforeDeath(self, person):
+        if (person.divDate =="not mentioned")or(person.death =="not mentioned"):
+            return True
+        else:
+            return gedUtil().dateCompare(person.death,person.divDate)
+	
+	
+	
+	
+    #US07 Less than 150 years old
+    def lessThan150Years(self, person):
+        return gedUtil.getAge(self, person) < 150
+
+    def validate_family(self,indList,famList):
+        for family in famList:
+            for ind in indList:
+                if(ind.familyC== family.famid):
+                    if ind.sex != 'M' and ind.sex != 'F':
+                        return False
+            if family.husband != "invalid/not mentioned" and family.husband.sex != 'M':
+                return False
+            if family.wife != "invalid/not mentioned" and family.wife.sex != 'F':
+                return False
+
+    #US08 Birth before marriage of parents
+    #US09 Birth before death of parents
+    def validBirth(self,indList, famList):
+        util = gedUtil()
+        for ind in indList:
+            if ind.familyC != "not mentioned":
+                father = None
+                fatherID = None
+                mother = None
+                motherID = None
+                fam = None
+                marriage = None
+
+                for family in famList:
+                    if family.famid == ind.familyC:
+                        fatherID = family.husband
+                        motherID = family.wife
+                        fam = family
+                        break
+
+                for inds in indList:
+                    if inds.indi == fatherID:
+                        father = inds
+                        marriage = inds.marDate
+                    if inds.indi == motherID:
+                        mother = inds
+                        marriage = inds.marDate
+            try:
+                if util.getDate(father.death) is not None and util.getDate(father.death) < util.getDate(ind.birth) - datetime.timedelta(days=266):
+                    print("Child is born more than 9 months after death of father")
+                    return False
+
+                if util.getDate(mother.death) is not None and util.getDate(mother.death) < util.getDate(ind.birth):
+                    print("Child is born after death of mother")
+                    return False
+
+                if util.dateCompare(util.getDate(marriage),util.getDate(ind.birth)):
+                    print("Child is born before marriage of parents")
+                    return False
+            except:
+                print("incomplete info")
+        return True
+
+    #US10 Marriage after 14
+    def validMarriage(self,indList, famList):
+        util = gedUtil()
+        current = datetime.datetime.today()
+        min_birth = datetime.datetime(current.year - 14, current.month, current.day)
+
+        for ind in indList:
+            if ind.familyC != "not mentioned":
+
+                #US17 No marriage to descendants
+                for family in famList:
+                    fatherID = []
+                    motherID = []
+                    descendantID = []
+                    for ind in ind.family:
+                        if family.famid == ind.familyC:
+                            fatherID.append(family.husband)
+                            motherID.append(family.wife)
+                            descendantID.append(ind)
+                            for ind in descendantID:
+                                for i in fatherID:
+                                    if ind.husbID == i:
+                                        print(ind + " is married to an ancestor")
+                                        return False
+                                for i in motherID:
+                                    if ind.wifeID == i:
+                                        print(ind + " is married to an ancestor")
+                                        return False
+
+        for family in famList:
+                husband = None
+                wife = None
+                for ind in indList:
+                    if ind.indi == family.husband:
+                        husband = ind
+                    if ind.indi == family.wife:
+                        wife = ind
+                    if husband is not None and wife is not None:
+                        break
+
+        for family in famList:
+            husband = None
+            wife = None
+            for ind in indList:
+                if ind.indi == family.husband:
+                    husband = ind
+                if ind.indi == family.wife:
+                    wife = ind
+                if husband is not None and wife is not None:
+                    break
+        try:
+            if util.getDate(husband.birth) > min_birth:
+                print(husband + " is married before 14 years old")
+                return False
+
+            if util.getDate(wife.birth) > min_birth:
+                print(wife + " is married before 14 years old")
+                return False
+        except:
+            print("incomplete data")
+        return True
+
+    #US29 List Deceased
+    def listDeceased(self,indList):
+        deceased = []
+        for ind in indList:
+            if ind.death != "not mentioned":
+                deceased.append(ind)
+        return deceased
+
+    #US30 List living married
+    def livingMarried(self,indList,famList):
+        living = []
+        for family in famList:
+            husband_id = family.husband
+            wife_id = family.wife
+            husband = None
+            wife = None
+
+            for ind in indList:
+                if ind.indi == husband_id and ind.death != "not mentioned":
+                    husband = ind
+                if ind.indi == wife_id and ind.death != "not mentioned":
+                    wife = ind
+            if wife is not None and husband is not None:
+                living.append(wife)
+                living.append(husband)
+        return living
--- GedRead.py
+++ GedRead.py
@@ -4,6 +4,9 @@
 from GedMembers import Valid
 from GedMembers import individual
 from GedMembers import family
+from GedHelper import gedHelper
+
+
 import copy
 import sys
 
@@ -30,7 +33,7 @@ def infoProcess(line:"Data line",type:"1 for indi 2 for fam"):# To get data in l
             Count += 1
         ListTemp[i[0]].insert(0,Count)
     # Save the Info Block
-    for j in range(1,Count):
+    for j in range(1,Count+1):
         tempBlock = []
         for item in ListTemp:
             if item[0] is j:
@@ -44,40 +47,7 @@ def infoProcess(line:"Data line",type:"1 for indi 2 for fam"):# To get data in l
     elif type ==2:
         getFamInfoFromBlocks(BlockList)
 
-def getIndInfoFromBlocks(blocks):
-    for i in blocks:
-        if len(indList) < indLength:
-            tempIndi = individual(None,None)
-            for j in i:
-                if j[4] == 'INDI':
-                    tempIndi.indi = j[2]
-                if j[2] == 'NAME':
-                    tempIndi.name = j[4]
-            indList.append(tempIndi)
-        else:
-            print("Maximum amount of individuals stored!\n")
 
-def getFamInfoFromBlocks(blocks):
-    for i in blocks:
-        if len(famList) < famLength:
-            tempFam = family(None,None)
-            for j in i:
-                if j[4] == 'FAM':
-                    tempFam.famid = j[2]
-                if j[2] == 'HUSB':
-                    tempFam.husband = j[4]
-                if j[2] == 'WIFE':
-                    tempFam.wife = j[4]
-            famList.append(tempFam)
-        else:
-            print("Maximum amount of families stored!\n")
-    #Search the name for them and add
-    for spouse in famList:
-        for person in indList:
-            if  person.indi == spouse.husband:
-                spouse.husbandN = person.name
-            if  person.indi == spouse.wife:
-                spouse.wifeN = person.name
 
 
 
@@ -90,22 +60,6 @@ def isValid(level:"tag level", tag:"tag name") -> str:
     else:
         return "N"
 
-def  validate_family(indList,famList):
-
-    for family in famList:
-
-        for ind in indList:
-
-            if(ind.family== family.famid):
-
-                if(ind.sex!='M' && ind.sex!='F' )
-                    return False
-        if(family.husband!="invalid/not mentioned" and family.husband.sex!='M' )
-            return False
-        if(family.wife!="invalid/not mentioned" and family.wife.sex!='F' )
-            return False
-
-
 def getNameByIndi(indi):
     re = "Invalid / Not Mentioned"
     for person in indList:
@@ -116,15 +70,8 @@ def getNameByIndi(indi):
 
 
 def readGed(file):
-    indList = []
-    famList = []
-    linedataList = []
-
-    indLength = 5000
-    famLength = 1000
-    validity = 'valid'
-	validate_family(indList,famList)
     try:
+        validity = 'valid'
         myGed = open(file, "r")
         gedLines = myGed.readlines()
         gedLines.append("END END END")
@@ -138,13 +85,6 @@ def readGed(file):
 
         infoProcess(linedataList,1)
         infoProcess(linedataList,2)
-        print("=====Individuals=====")
-        for i in indList:
-            i.printBriefInfo()
-        print("=====Family=====")
-        for j in famList:
-            print("FamilyID:"+j.famid+ " Husband Name:"+ getNameByIndi(j.husband) + " Wife Name:" + getNameByIndi(j.wife))
-
         myGed.close()
     except:
         print("Invalid file")
@@ -154,47 +94,103 @@ def readGed(file):
         return validity
 
 
-def readGedTest(file, temp, ind):
-    indList = []
-    famList = []
-    linedataList = []
+def getIndInfoFromBlocks(blocks):
+    for infoBlock in blocks:
+        if len(indList) < indLength:
+            tempIndi = individual(None)
+            for index in range(len(infoBlock)):
+                # deal with properties here
+                if infoBlock[index][4] == 'INDI':
+                    tempIndi.indi = infoBlock[index][2]#Kt
+                if infoBlock[index][2] == 'NAME':
+                    tempIndi.name = infoBlock[index][4]#Kt
+                if infoBlock[index][2] == 'BIRT\n':
+                    tempIndi.birth = infoBlock[index+1][4]#Kt
+                if infoBlock[index][2] == 'DEAT':
+                    tempIndi.death = infoBlock[index+1][4]#Kt
+                # Marriage/Divorce date is about to add
+                if infoBlock[index][2] == 'FAMC':
+                    tempIndi.familyC = infoBlock[index][4]#Na
+                #if j[2] == 'FAMS':
+                #    tempIndi.familyS.append(j[4])
+            indList.append(tempIndi)
+        else:
+            print("Maximum amount of individuals stored!\n")
 
-    indLength = 5000
-    famLength = 1000
-    try:
-        myGed = open(file, "r")
-        gedLines = myGed.readlines()
-        gedLines.append("END END END")
-        for line in gedLines:
-            #Do line cut and store the whole line
-            line = line + " "
-            linedata = [line[0:1], line[2:line.index(" ", 2)], "Valid", line[line.index(" ", 2) + 1:-1]]
-            linedata[2] = isValid(linedata[0], linedata[1])
-            linedata[3] = linedata[3].replace("\n","")
-            linedataList.append(linedata)
+def getFamInfoFromBlocks(blocks):
+    for infoBlock in blocks:
+        if len(famList) < famLength:
+            tempFam = family(None)
+            for index,infoLine in enumerate(infoBlock):
+                if infoLine[4] == 'FAM':
+                    tempFam.famid = infoLine[2]
+                if infoLine[2] == 'HUSB':
+                    tempFam.husband = infoLine[4]
+                if infoLine[2] == 'WIFE':
+                    tempFam.wife = infoLine[4]
+                if infoLine[2] == 'MARR\n':
+                    tempFam.marDate = infoBlock[index+1][4]
+                if infoLine[2] == '_SEPR\n':
+                    tempFam.divDate = infoBlock[index+1][4]
+            famList.append(tempFam)
+        else:
+            print("Maximum amount of families stored!\n")
+    #Search the name for them and add
+    #Read Family data and add it to individual
+    for fam in famList:
+        for person in indList:
+            if  person.indi == fam.husband:
+                fam.husbandN = person.name
+                person.marDate = fam.marDate
+                person.divDate = fam.divDate
+            if  person.indi == fam.wife:
+                fam.wifeN = person.name
+                person.marDate = fam.marDate
+                person.divDate = fam.divDate
 
-        infoProcess(linedataList,1)
-        infoProcess(linedataList,2)
-        print("=====Individuals=====")
+
+def delItem(person,list):
+    for p in list:
+        if p.indi is person.indi:
+            list.remove(p)
+    return list
+
+
+def GedReader(file):
+    if readGed(file):
+        gh = gedHelper()
+        outputindList = copy.deepcopy(indList)
+        outputfamList = copy.deepcopy(famList)
+        #put all our user story here.
+        gh.validate_family(indList,famList)
+        gh.validBirth(indList,famList)
+        gh.validMarriage(indList,famList)
         for i in indList:
+            #if gh.datebeforeCurrentdate(i) == False:
+            #   delItem(i,outputindList)
+            if gh.birthBeforeMarriage(i) == False:
+                delItem(i,outputindList)
+            if gh.birthBeforeDeath(i) == False:
+                delItem(i,outputindList)
+            if gh.marriageBeforeDivorce(i) == False:
+                delItem(i,outputindList)
+	    if gh.marriageBeforeDeath(i) == False:
+                delItem(i,outputindList)
+	    if gh.marriageBeforeDeath(i) == False:
+                delItem(i,outputindList)
+            #if gh.lessThan150Years(i) == False:
+            #   delItem(i,outputindList)
+        
+        # for f in famList:
+
+
+        print("=====Individuals=====")
+        for i in outputindList:
             i.printBriefInfo()
         print("=====Family=====")
         for j in famList:
             print("FamilyID:"+j.famid+ " Husband Name:"+ getNameByIndi(j.husband) + " Wife Name:" + getNameByIndi(j.wife))
 
-        if ind == True:
-            temp = indList
-        if ind == False:
-            temp = famList
-        myGed.close()
-    except:
-        print("Invalid file")
-        myGed.close()
-    finally:
-        return temp
-
-
-
 
 if __name__ == '__main__':
-    readGed("Group 5 GED.ged")
+    GedReader(".\\TestGed\\MarriageAndDivorce.ged")
