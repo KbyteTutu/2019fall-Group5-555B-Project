98e4cb1f51c197517a16f26a7792c89177d075c5
KbyteTutu <vctu@vip.qq.com>
2019-11-07 14:40:37 -0500

    3

--- GedHelper.py
+++ GedHelper.py
@@ -1,4 +1,5 @@
-__author__= 'Group5'
+__author__ = 'Group5'
+
 
 import datetime
 from GedMembers import Valid
@@ -6,75 +7,74 @@ from GedMembers import individual
 from GedMembers import family
 from GedUtil import gedUtil
 
-class gedHelper(object):
 
+class gedHelper(object):
 
     def __init__(self):
         pass
-    #US01 Date before CurrentDate
+    # US01 Date before CurrentDate
+
     def datebeforeCurrentdate(self, person):
         util = gedUtil()
         current = datetime.datetime.today()
-        if (person.birth !="not mentioned"):
-            return gedUtil().dateCompare(util.getDate(current),person.birth)
-        elif (person.marDate !="not mentioned"):
-            return gedUtil().dateCompare(person.marDate,util.getDate(current))
-        elif (person.death !="not mentioned"):
-            return gedUtil().dateCompare(person.death,util.getDate(current))
-        elif (person.divDate !="not mentioned"):
-            return gedUtil().dateCompare(person.divDate,util.getDate(current))
+        if (person.birth != "not mentioned"):
+            return gedUtil().dateCompare(util.getDate(current), person.birth)
+        elif (person.marDate != "not mentioned"):
+            return gedUtil().dateCompare(person.marDate, util.getDate(current))
+        elif (person.death != "not mentioned"):
+            return gedUtil().dateCompare(person.death, util.getDate(current))
+        elif (person.divDate != "not mentioned"):
+            return gedUtil().dateCompare(person.divDate, util.getDate(current))
         else:
             return 0
-    
-    #US02 Birth before Marriage
+
+    # US02 Birth before Marriage
     def birthBeforeMarriage(self, person):
-        if (person.birth =="not mentioned")or(person.marDate =="not mentioned"):
+        if (person.birth == "not mentioned")or(person.marDate == "not mentioned"):
             return True
         else:
-            return gedUtil().dateCompare(person.marDate,person.birth)
+            return gedUtil().dateCompare(person.marDate, person.birth)
 
-    #US03 Birth before death
+    # US03 Birth before death
     def birthBeforeDeath(self, person):
-        if (person.birth =="not mentioned")or(person.death =="not mentioned"):
+        if (person.birth == "not mentioned")or(person.death == "not mentioned"):
             return True
         else:
-            return gedUtil().dateCompare(person.death,person.birth)
-        
-    #US04 Marriage before divorce
+            return gedUtil().dateCompare(person.death, person.birth)
+
+    # US04 Marriage before divorce
     def marriageBeforeDivorce(self, person):
-        if (person.marDate =="not mentioned")or(person.divDate =="not mentioned"):
+        if (person.marDate == "not mentioned")or(person.divDate == "not mentioned"):
             return True
         else:
-            return gedUtil().dateCompare(person.divDate,person.marDate)
+            return gedUtil().dateCompare(person.divDate, person.marDate)
 
-	#US05 Marriage before death
+        # US05 Marriage before death
     def marriageBeforeDeath(self, person):
-        if (person.marDate =="not mentioned")or(person.death =="not mentioned"):
+        if (person.marDate == "not mentioned")or(person.death == "not mentioned"):
             return True
         else:
-            return gedUtil().dateCompare(person.death,person.marDate)
-			
-			
-	#US06 Divorce before death
-    def divorceBeforeDeath(self, person):
-        if (person.divDate =="not mentioned")or(person.death =="not mentioned"):
-            return True
-        else:
-            return gedUtil().dateCompare(person.death,person.divDate)
-	
-	
+            return gedUtil().dateCompare(person.death, person.marDate)
 
-    #US07 Less than 150 years old
+        # US06 Divorce before death
+
+    def divorceBeforeDeath(self, person):
+        if (person.divDate == "not mentioned")or(person.death == "not mentioned"):
+            return True
+        else:
+            return gedUtil().dateCompare(person.death, person.divDate)
+
+    # US07 Less than 150 years old
+
     def lessThan150Years(self, person):
         return gedUtil.getAge(self, person) < 150
 
-
-    #Does not work until implementation of FamilyC
-    def validate_family(self,indList,famList):
+    # Does not work until implementation of FamilyC
+    def validate_family(self, indList, famList):
         for family in famList:
             for ind in indList:
                 for child in ind.familyC:
-                    if(child== family.famid):
+                    if(child == family.famid):
                         if ind.sex != 'M' and ind.sex != 'F':
                             return False
             if family.husband != "invalid/not mentioned" and family.husband.sex != 'M':
@@ -82,10 +82,10 @@ class gedHelper(object):
             if family.wife != "invalid/not mentioned" and family.wife.sex != 'F':
                 return False
 
-    #US08 Birth before marriage of parents
-    #US09 Birth before death of parents
-    def validBirth(self,indList, famList):
-        return_flag = True 
+    # US08 Birth before marriage of parents
+    # US09 Birth before death of parents
+    def validBirth(self, indList, famList):
+        return_flag = True
         util = gedUtil()
         for ind in indList:
             if ind.familyC != "not mentioned":
@@ -113,28 +113,29 @@ class gedHelper(object):
 
                 try:
                     if util.getDate(father.death) is not None and util.getDate(father.death) < util.getDate(ind.birth) - datetime.timedelta(days=266):
-                        print("Child is born more than 9 months after death of " + father.name)
+                        print(
+                            "Child is born more than 9 months after death of " + father.name)
                         return_flag = False
 
-                    
                     if util.getDate(mother.death) is not None and util.getDate(mother.death) < util.getDate(ind.birth):
                         print("Child is born after death of " + mother.name)
                         return_flag = False
 
-                
                     if util.getDate(ind.birth) < util.getDate(marriage):
-                        print("Child is born before marriage of " + father.name + " and " + mother.name)
+                        print("Child is born before marriage of " +
+                              father.name + " and " + mother.name)
                         return_flag = False
                 except:
                     print("incomplete info")
         return return_flag
 
-    #US10 Marriage after 14
-    def validMarriage(self,indList, famList):
+    # US10 Marriage after 14
+    def validMarriage(self, indList, famList):
         return_flag = True
         util = gedUtil()
         current = datetime.datetime.today()
-        min_birth = datetime.datetime(current.year - 14, current.month, current.day)
+        min_birth = datetime.datetime(
+            current.year - 14, current.month, current.day)
 
         for family in famList:
             husband = None
@@ -151,8 +152,7 @@ class gedHelper(object):
             if util.getDate(husband.birth) > min_birth:
                 print(husband + " is married before 14 years old")
                 return_flag = False
-            
-            
+
             if util.getDate(wife.birth) > min_birth:
                 print(wife + " is married before 14 years old")
                 return_flag = False
@@ -160,8 +160,26 @@ class gedHelper(object):
             print("incomplete data")
         return return_flag
 
-    #US19 First Cousins Should Not Marry to be continued
-    def cousinsMarried(self,indList,famList):
+    # US13 Siblings spacing
+    def sameTimeBirth(self, PersonA, PersonB):
+        util = gedUtil()
+        if (PersonA.birth == "not mentioned")or(PersonB.birth == "not mentioned"):
+            return True
+
+        Abirth = util.getDate(PersonA.birth)
+        Bbirth = util.getDate(PersonB.birth)
+
+        if (Abirth-Bbirth).days <= 2:
+            return True
+        else:
+            return False
+
+    
+
+    # US14 Multiple births <= 5
+
+    # US19 First Cousins Should Not Marry to be continued
+    def cousinsMarried(self, indList, famList):
         return_flag = True
         util = gedUtil()
 
@@ -176,18 +194,18 @@ class gedHelper(object):
                 if husband is not None and wife is not None:
                     break
 
-    #US20 Aunts and Uncles to be continued
+    # US20 Aunts and Uncles to be continued
 
-    #US29 List Deceased
-    def listDeceased(self,indList):
+    # US29 List Deceased
+    def listDeceased(self, indList):
         deceased = []
         for ind in indList:
             if ind.death != "not mentioned":
                 deceased.append(ind)
         return deceased
 
-    #US30 List living married
-    def livingMarried(self,indList,famList):
+    # US30 List living married
+    def livingMarried(self, indList, famList):
         living = []
         for family in famList:
             husband_id = family.husband
@@ -204,6 +222,3 @@ class gedHelper(object):
                 living.append(wife)
                 living.append(husband)
         return living
-
-
-
